64f727e91a79651b6a10f3e164d7622b5232bd1d
EvelinaSarg <evelina.sargsyan@gmail.com>
2024-03-26 14:57:57 +0000

    merge conflict

--- earthquake.py
+++ earthquake.py
@@ -80,32 +80,8 @@ end_date = st.date_input('End date', value=datetime.now(), min_value=start_date,
 
 if (end_date - start_date).days > 50:
     st.error('The date range must not exceed 50 days.')
-<<<<<<< HEAD
 else:
     # Fetch data and prepare the map
-=======
-
-# API call
-def get_data(start_date, end_date):
-    url = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}"
-    response = requests.get(url)
-    return response.json()
-
-# Function to extract places and coordinates
-def extract_places(data):
-    places = []
-    for item in data['features']:
-        place = item['properties']['place']
-        longitude, latitude = item['geometry']['coordinates'][0:2]
-        places.append({'place': place, 'latitude': latitude, 'longitude': longitude})
-    return places
-
-# Display data on a map
-if st.button('Show Map'):
-    if start_date > end_date:
-        st.error('Error: Start date must be before the end date.')
-else:
->>>>>>> 3383550ccbc984fcc0d639295046c9f059124ea8
     data = get_data(start_date, end_date)
     earthquakes = extract_data(data)
     df = pd.DataFrame(earthquakes)
